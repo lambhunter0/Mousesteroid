596dfe8a0f8127f5128a096105a90c60c82bf994
lambhunter0 <george.theophanous@hotmail.com>
2019-09-12 18:56:03 -0400

    added character controller creation "kit"
    added formalized method of creating a character controller in a data driven way.

---
+++ Assets/Mousesteroid/PlayerController/Scripts/CharacterData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+
+namespace PersonalProjects.Mouseteroid.Player
+{
+    [Serializable]
+    public class CharacterData 
+    {
+        [Header("Movement")]
+        [SerializeField] private float movementSpeed;
+        [Header("Health")]
+        [SerializeField] private int maxHealth;
+        [SerializeField] private int maxLives;
+
+        public float MovementSpeed { get { return movementSpeed; } }
+        public int MaxHealth { get { return maxHealth; } }
+        public int MaxLives { get { return maxLives; } }
+    }
+}
---
+++ Assets/Mousesteroid/PlayerController/Scripts/CharacterDataTemplate.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace PersonalProjects.Mouseteroid.Player
+{
+    [CreateAssetMenu(fileName = "New CharacterDataTemplate", menuName = "Data/CharacterTemplate")]
+    public class CharacterDataTemplate : ScriptableObject
+    {
+        [SerializeField] private CharacterData characterData;
+
+        public CharacterData CharacterData { get { return characterData; } }
+    }
+}
